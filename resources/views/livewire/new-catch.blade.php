4ec4e393fb3bfa511b598f6a350516d1de53be2a
Felix Auer <felix.auer@timetoact.at>
2026-07-16 16:46:29 +0200

    fix upload

--- resources/views/livewire/new-catch.blade.php
+++ resources/views/livewire/new-catch.blade.php
@@ -392,9 +392,21 @@ new #[Layout('layouts.dex')] class extends Component
             </div>
 
             {{-- Photo --}}
-            <div>
+            <div x-data>
                 <label class="block text-sm font-medium mb-1">{{ __('Photo (optional)') }}</label>
-                <input type="file" wire:model="photo" accept="image/*" class="w-full text-sm">
+
+                <input type="file" x-ref="cameraInput" wire:model="photo" accept="image/*" capture="environment" class="hidden">
+                <input type="file" x-ref="galleryInput" wire:model="photo" accept="image/*" class="hidden">
+
+                <div class="flex gap-3">
+                    <button type="button" @click="$refs.cameraInput.click()" class="px-3 py-1.5 text-sm rounded-lg border border-gray-300 dark:border-gray-700">
+                        📷 {{ __('Take photo') }}
+                    </button>
+                    <button type="button" @click="$refs.galleryInput.click()" class="px-3 py-1.5 text-sm rounded-lg border border-gray-300 dark:border-gray-700">
+                        🖼️ {{ __('Choose from gallery') }}
+                    </button>
+                </div>
+
                 @if ($photo)
                     <img src="{{ $photo->temporaryUrl() }}" class="mt-2 h-32 w-32 object-cover rounded-lg">
                 @elseif ($existingPhotoUrl)
